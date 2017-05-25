a14074be64e9f17f21e5dd237535f14c285c8d28
Joyce Fang <jofang@ucsd.edu>
2017-05-25 13:14:50 -0700

    logging in works, need to check for persistence

--- app/js/index.js
+++ app/js/index.js
@@ -12,7 +12,7 @@ var database = firebase.database();
 
 
 // adds user to firebase if signup botton is clicked
-function update() {
+function signup() {
 
     var email = $("#email").val();
     var password = $("#password").val();
@@ -43,6 +43,27 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
   }
 });
 
+function login() {
+
+    var email = $("#email").val();
+    var password = $("#password").val();
+    
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        // displays a pop up alert when an error occurs
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else if(errorCode == 'auth/email-already-in-use') {
+            alert('The email is already in use.');
+        } else if(errorCode == 'auth/invalid-email'){
+            alert('The email is invalid.');
+        } 
+    });
+}
 $(document).ready(function() {
 
     var ref = database.ref();
